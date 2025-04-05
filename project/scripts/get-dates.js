3bf38f9d10ddc1e65cdde24d6436fb808f7d209d
ChrisOsunaE <cosunaescalona@byupathway.edu>
2025-04-05 09:44:57 -0700

    Add site plan structure, styles, and dynamic date display

---
+++ project/scripts/get-dates.js
@@ -0,0 +1,6 @@
+// currentyear
+const currentyear = document.getElementById('currentyear');
+currentyear.textContent = new Date().getFullYear();
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = document.lastModified;
