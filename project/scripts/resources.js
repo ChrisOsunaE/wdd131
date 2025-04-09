2bb8baf57a17884a68ec2300d38166bc3c8edb58
chrisosuna <obedosunae@gmail.com>
2025-04-09 15:54:15 -0700

    Update script references in HTML files; add resources page and JavaScript for mental health resources

---
+++ project/scripts/resources.js
@@ -0,0 +1,33 @@
+const mentalHealthResources = {
+  discordServers: [
+    {
+      name: "ChillCorner | Mental Health 💤",
+      description: "One of the leading Discord servers dedicated to mental health, offering a comprehensive guide to mental wellness and an active, supportive community.",
+      url: "https://discord.com/servers/chillcorner-mental-health-961371951748767797"
+    },
+    {
+      name: "Adult Mental Health",
+      description: "Geared toward adults seeking an environment to discuss mental health issues, this server provides a space to share experiences and connect with others facing similar challenges.",
+      url: "https://discord.me/adultmentalhealth"
+    }
+  ],
+  onlineSupportGroups: [
+    {
+      name: "Anxiety and Depression Association of America (ADAA) Online Support Group",
+      description: "Free online communities where people suffering from mental health disorders can find support and share their stories.",
+      url: "https://adaa.org/find-help/support"
+    },
+    {
+      name: "By My Side",
+      description: "Offers free emotional support and counseling from trained listeners through online text chats.",
+      url: "https://bymyside.support/"
+    }
+  ],
+  informativeWebsites: [
+    {
+      name: "Centers for Disease Control and Prevention (CDC) - Providing Support for Worker Mental Health",
+      description: "Guidance on how employers can support workers' mental health, including strategies for managing stress and preventing burnout.",
+      url: "https://www.cdc.gov/mental-health/caring/providing-support-for-workers-and-professionals.html"
+    },
+  ]
+};
